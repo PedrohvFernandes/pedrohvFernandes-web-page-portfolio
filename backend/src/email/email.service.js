886c42b7e02efa078cdbd601f071b8baac8e5850
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-04-25 17:29:56 -0300

    Update email.service.js

--- backend/src/email/email.service.js
+++ backend/src/email/email.service.js
@@ -7,6 +7,11 @@ export class EmailService {
   async sendMail({ to, subject, html }) {
     const info = await this.#transport.sendMail({
       from: this.#transport.options.from,
+      priority: 'high', // Prioridade alta
+      headers: {
+        'X-Priority': '1', // Valor de prioridade personalizado
+        'X-MSMail-Priority': 'High' // Outro cabeçalho de prioridade personalizado para clientes do Outlook
+      },
       to,
       subject,
       html
