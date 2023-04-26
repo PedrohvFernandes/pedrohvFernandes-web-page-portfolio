9c2527f1d9aade2341421866727cf41b269043a1
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-04-26 15:47:31 -0300

    Update email.service.js

--- Web/src/js/service/email.service.js
+++ Web/src/js/service/email.service.js
@@ -16,7 +16,7 @@ export function sendMail() {
       if (
         !email.trim() ||
         !name.trim() ||
-        !idea.trim ||
+        !idea.trim() ||
         !email ||
         !name ||
         !idea ||
