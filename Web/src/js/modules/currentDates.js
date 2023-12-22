fc97100b4cd8737a424cc418fea2b8f1320c064c
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-12-21 22:02:14 -0300

    Update currentDates.js

--- Web/src/js/modules/currentDates.js
+++ Web/src/js/modules/currentDates.js
@@ -7,17 +7,18 @@
 // }
 // myAge()
 
-function checkAge(currentMonth, birthMonth, age, birthDay){
+function checkAge(currentMonth, birthMonth, age, birthDay) {
   //Se mes atual for menor que o nascimento, nao fez aniversario ainda;
   if (currentMonth < birthMonth) {
     return --age
-  } 
+  }
   //Se estiver no mes do nascimento, verificar o dia
   if (currentMonth === birthMonth) {
     if (new Date().getDate() < birthDay) {
       //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
       return --age
     }
+    return age
   }
 }
 
@@ -29,15 +30,15 @@ export function currentAge(birthDate) {
   // Pegando a data de nascimento e criando um vetor de tres indices(dia, mes e ano)
   let birthYearParts = birthDate.split('/')
   // Capturando a data de nascimento em cada variavel
-  let birthDay = birthYearParts[0]
-  let birthMonth = birthYearParts[1]
-  let birthYear = birthYearParts[2]
+  let birthDay = Number(birthYearParts[0])
+  let birthMonth = Number(birthYearParts[1])
+  let birthYear = Number(birthYearParts[2])
   // age da pessoa
   let age = currentYear - birthYear
   // A gente pega o mes + 1 porque o date pega mes anterior ao atual
   let currentMonth = currentDate.getMonth() + 1
   const ageCurrent = checkAge(currentMonth, birthMonth, age, birthDay)
-  document.getElementById('years').innerHTML = ageCurrent;
+  document.getElementById('years').innerHTML = ageCurrent
 }
 
 export function currentYear() {
